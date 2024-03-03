f9cc097222cb42002f70a2aef9a420f1e28709c7
mistidevs <mistidevs@gmail.com>
2024-03-03 15:53:52 +0300

    code(1-hbnb.py, 1-hbnb.html, 1-hbnb.js) : Amenities drop down menu

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+  $('li input[type="checkbox"]').each(function() {
+      const amenityId = $(this).data('id');
+      const amenityName = $(this).data('name');
+  });
+});
