23526c6a5873c26034d0f90a06f961b0cf7a8cc5
mistidevs <mistidevs@gmail.com>
2024-03-03 21:33:37 +0300

    code(3-headers.css, 2-hbnb.js) : API reconfiguration

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,20 +1,31 @@
-$(document).ready(function() {
-  $('li input[type="checkbox"]').each(function() {
-      const amenityId = $(this).data('id');
-      const amenityName = $(this).data('name');
-  });
-  $.ajax
-});({
-  url: 'http://0.0.0.0:5001/api/v1/status/',
-  type: 'GET',
-  success: function(data) {
-    if (data.status === 'OK') {
-      $('#api_status').addClass('available');
+$(document).ready(function () {
+  const amens = {};
+  $('input:checkbox').click(function () {
+    $(this).each(function () {
+      if (this.checked) {
+        amens[$(this).data('id')] = $(this).data('name');
+      } else {
+        delete amens[$(this).data('id')];
+      }
+    });
+    if (Object.values(amens).length > 0) {
+      $('.amenities h4').text(Object.values(amens).join(', '));
     } else {
-      $('#api_status').removeClass('available');
+      $('.amenities h4').html('&nbsp');
     }
-  },
-  error: function(error) {
-    console.log(error);
-  }
-})
+  });
+  $.ajax({
+    url: 'http://0.0.0.0:5001/api/v1/status/',
+    type: 'GET',
+    success: function(data) {
+      if (data.status === 'OK') {
+        $('#api_status').addClass('available');
+      } else {
+        $('#api_status').removeClass('available');
+      }
+    },
+    error: function(error) {
+      console.log(error);
+    }
+  });
+});
