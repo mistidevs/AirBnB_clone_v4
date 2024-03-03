6ecfb1478b1e07cfb05699d8fe1a0a313ecf45d0
Misti Ubuntu <mistidevs@gmail.com>
2024-03-03 17:11:59 +0300

    code(./web_dynamic) : amenities handling ; frustration(6-fiters.css) : popover gap and no ellipsis

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,32 +1,17 @@
-$(document).ready(function() {
-  // Initialize an empty list to store amenity IDs
-  let selectedAmenities = [];
-
-  // Listen for changes on each checkbox
-  $('input[type="checkbox"]').change(function() {
-    const amenityId = $(this).data('id');
-
-    // Check if checkbox is checked
-    if ($(this).is(':checked')) {
-      // Add amenity ID to the list if not already present
-      if (!selectedAmenities.includes(amenityId)) {
-        selectedAmenities.push(amenityId);
+$(document).ready(function () {
+  const amens = {};
+  $('input:checkbox').click(function () {
+    $(this).each(function () {
+      if (this.checked) {
+        amens[$(this).data('id')] = $(this).data('name');
+      } else {
+        delete amens[$(this).data('id')];
       }
-    } else {
-      // Remove amenity ID from the list
-      const index = selectedAmenities.indexOf(amenityId);
-      if (index !== -1) {
-        selectedAmenities.splice(index, 1);
-      }
-    }
-
-    // Update the h4 tag with the list of selected amenity names
-    const amenityNames = selectedAmenities.map((id) => {
-      const amenity = $(`[data-id="${id}"]`).data('name');
-      return amenity;
     });
-
-    const amenityListText = amenityNames.join(', ');
-    $('.amenities h4').text(amenityListText || 'No Amenities Selected');
+    if (Object.values(amens).length > 0) {
+      $('.amenities h4').text(Object.values(amens).join(', '));
+    } else {
+      $('.amenities h4').html('&nbsp');
+    }
   });
 });
