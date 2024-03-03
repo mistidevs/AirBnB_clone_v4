6a3fda99993b357da25049ff10df407cdd3decd6
mistidevs-node <mistidevs@gmail.com>
2024-03-03 05:33:16 -0800

    code (1-hbnb.js) : JS for functionality

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,6 +1,19 @@
 $(document).ready(function() {
-  $('li input[type="checkbox"]').each(function() {
-      const amenityId = $(this).data('id');
-      const amenityName = $(this).data('name');
+  let amenityIDs = [];
+  function updateAmenities() {
+    var amenitiesList = amenityIDs.join(', ');
+    $('.amenities h4').text(amenitiesList);
+  };
+  $('input[type="checkbox"]').change(function () {
+    var amenityID = $.(this).data('id');
+    if ($(this).prop('checked')) {
+      amenityIDs.push(amenityID);
+    } else {
+      var index = amenityIDs.indexOf(amenityID);
+      if (index !== -1) {
+        amenityIDs.splice(index, 1);
+      }
+    }
+    updateAmenities();
   });
 });
